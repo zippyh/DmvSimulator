28e86b57c0735c807ec1b1c6c2c38bdf674866af
JeremyGerm <jeremyrwiening@gmail.com>
2024-03-06 12:26:36 -0500

    even meaner message

--- DmvSimulator.java
+++ DmvSimulator.java
@@ -11,7 +11,7 @@ public class DmvSimulator {
             System.out.println("Calling number: " + numberCalled);
 
             if (numberCalled == userNumber) {
-                System.out.println("You do NOT have the required paperwork! Be a competent person and bring everything you need before waiting in line!!!");
+                System.out.println("You do NOT have the required paperwork! Be a competent person and bring everything you need before waiting in line!!!\n Jeez you are incompetent!!!");
                 break;
             }
             numberCalled++;
